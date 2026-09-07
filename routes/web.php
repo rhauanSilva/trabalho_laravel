76a1283bafd3d58b95cb800ced7c35f2b81f2594
RhauanSilva <engs-rhauansilva@camporeal.edu.br>
2026-09-07 11:13:37 -0300

    criação das rotas com os parametros pedidos.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Exibindo o produto ID: " . $id;
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Exibindo a categoria ID: " . $id;
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Exibindo o usuário ID: " . $id;
+});
