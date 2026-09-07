a75312d4e06471d31abac4a5b853db06b3e3f487
LeticiaBorgesCardoso <leticiacardosoborges07@gmail.com>
2026-09-07 11:19:17 -0300

    ATV 2: Crie as rotas com parametro

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Página de Contato';
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Exibindo o produto: " . $id;
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Exibindo a categoria: " . $id;
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Exibindo o usuário: " . $id;
+});
