b1d742db8e04617c07678b9ff82aac5dd258a748
RhauanSilva <engs-rhauansilva@camporeal.edu.br>
2026-09-07 16:06:15 -0300

    commits pela metade pra dar uma pausa

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model
 {
-    protected $fillable = ['nome', 'curso'];
+    protected $fillable = ['nome', 'curso', 'curso_id'];
+
+    public function cursoRelacionado()
+    {
+        return $this->belongsTo(Curso::class, 'curso_id');
+    }
 }
